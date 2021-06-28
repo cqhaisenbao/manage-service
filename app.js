7dcfd7ddd687930dab6ef8d70ca5e4fc7bf805c0
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-29 00:51:58 +0800

    封装路由

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const log4js = require('./utils/log4j')
-
-const users = require('./routes/users')
+const routing = require('./routes')
 
 // error handler
 onerror(app)
@@ -20,21 +18,14 @@ app.use(bodyparser({
 app.use(json())
 app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-    extension: 'pug'
-}))
-
 // logger
 app.use(async (ctx, next) => {
     await next()
     log4js.info('xx')
 })
 
-app.use(() => {
-    ctx.body = 'hello'
-})
 // routes
-app.use(users.routes(), users.allowedMethods())
+routing(app)
 
 // error-handling
 app.on('error', (err, ctx) => {
