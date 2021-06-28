d872266faf08a25ac3c286f46cc720984ebe0f26
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-28 21:35:02 +0800

    log4j打印日志

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const log4js = require('./utils/log4j')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -14,31 +14,31 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+    extension: 'pug'
 }))
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    await next()
+    log4js.info('xx')
 })
 
+app.use(() => {
+    ctx.body = 'hello'
+})
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    log4js.error(`${err.stack}`)
 });
 
 module.exports = app
